0b606753603ce3231b9b81bf78990ef5535f002b
Jeonghwan Kim <51739985+hwan20202@users.noreply.github.com>
2026-05-24 17:47:32 +0900

    feat : GCP secret 참조 및 환경변수 적용

--- src/main/java/com/kosa/noticeserver/NoticeServerApplication.java
+++ src/main/java/com/kosa/noticeserver/NoticeServerApplication.java
@@ -1,13 +1,17 @@
 package com.kosa.noticeserver;
 
+import com.kosa.noticeserver.config.EnvConfigInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class NoticeServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(NoticeServerApplication.class, args);
+        new SpringApplicationBuilder(NoticeServerApplication.class)
+                .initializers(new EnvConfigInitializer())
+                .run(args);
     }
 
 }
--- src/main/java/com/kosa/noticeserver/config/FirebaseConfig.java
+++ src/main/java/com/kosa/noticeserver/config/FirebaseConfig.java
@@ -7,10 +7,8 @@ import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
 import java.io.IOException;
-import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
