d5fcd50e800e6ab6e5fd4ff2f5f0491ef4d042c7
Jeonghwan Kim <51739985+hwan20202@users.noreply.github.com>
2026-05-25 16:12:03 +0900

    fix : 삭제된 의존성 제거

--- src/main/java/com/kosa/noticeserver/NoticeServerApplication.java
+++ src/main/java/com/kosa/noticeserver/NoticeServerApplication.java
@@ -1,9 +1,7 @@
 package com.kosa.noticeserver;
 
-import com.kosa.noticeserver.config.EnvConfigInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class NoticeServerApplication {
